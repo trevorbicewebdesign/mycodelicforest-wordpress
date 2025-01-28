61dabd64b233484fb44e9287da2691a591fd6cf1
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-01-27 18:07:39 -0800

    Update mycodelic-forest.php

--- wp-content/plugins/mycodelic-forest/mycodelic-forest.php
+++ wp-content/plugins/mycodelic-forest/mycodelic-forest.php
@@ -13,22 +13,26 @@ define('MYCO_CORE_ABS_PATH', WP_CONTENT_DIR . "/plugins/mycodelic-forest/");
 
 require_once(MYCO_CORE_ABS_PATH . 'classes/class-core.php');
 require_once(MYCO_CORE_ABS_PATH . 'classes/class-forms.php');
+require_once(MYCO_CORE_ABS_PATH . 'classes/class-shortcodes.php');
 
  
 class MycodelicForestInit {
     public $version = '0.0.1';
     public $MycodelicForestCore;
     public $MycodelicForestForms;
+    public $MycodelicForestShortcodes;
 
     public function __construct() {
         $this->MycodelicForestCore = new MycodelicForestCore();
         $this->MycodelicForestForms = new MycodelicForestForms();
+        $this->MycodelicForestShortcodes = new MycodelicForestShortcodes();
     }
 
     public function init()
     {
         $this->MycodelicForestCore->init();
         $this->MycodelicForestForms->init();
+        $this->MycodelicForestShortcodes->init();
     }
 }
 
