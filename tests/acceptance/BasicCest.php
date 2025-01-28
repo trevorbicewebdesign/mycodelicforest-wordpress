b9c1d97aaab1638dfaf2802b1859e2d328a3080f
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-01-27 17:54:10 -0800

    Update BasicCest.php

--- tests/acceptance/BasicCest.php
+++ tests/acceptance/BasicCest.php
@@ -26,6 +26,7 @@ class BasicCest
     public function faqsPageIsVisible(AcceptanceTester $I)
     {
         $I->amOnPage("/faqs");
+        $I->wait(1);
         $I->see("FAQs");
         $I->takeFullPageScreenshot("faqs-page");
     }
@@ -33,6 +34,7 @@ class BasicCest
     public function resourcesPageIsVisible(AcceptanceTester $I)
     {
         $I->amOnPage("/resources");
+        $I->wait(1);
         $I->see("Resources");
         $I->takeFullPageScreenshot("resources-page");
     }
@@ -40,6 +42,7 @@ class BasicCest
     public function registerPageIsVisible(AcceptanceTester $I)
     {
         $I->amOnPage("/register");
+        $I->wait(1);
         $I->see("Register");
         $I->takeFullPageScreenshot("register-page");
     }
@@ -47,8 +50,33 @@ class BasicCest
     public function contactPageIsVisible(AcceptanceTester $I)
     {
         $I->amOnPage("/contact");
+        $I->wait(1);
         $I->see("Contact Us");
         $I->takeFullPageScreenshot("contact-page");
+
+        $I->see("Name", "legend");
+        $I->see("First", "label");
+        $I->see("Last", "label");
+        $I->see("Email", "legend");
+        $I->see("Subject", "legend");
+        $I->see("Message", "legend");
+        $I->see("Submit", "button");
+
+        $I->click("Submit");
+
+        $I->see("The First Name field is required.");
+
+        $I->fillField("#input_4_1_3", "John");
+        $I->fillField("#input_4_1_6", "Doe");
+        $I->fillField("#input_4_5", "test.smith@mailinator.com");
+        $I->fillField("#input_4_3", "Test Subject");
+        $I->fillField("#input_4_4", "Test Message");
+        $I->click("Submit");
+
+        $I->wait(1);
+
+        $I->see("Contact Form Was Submitted");
+
     }
 
 
