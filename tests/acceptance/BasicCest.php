12fa133a83197712a8adfe8e6983cc445ce03d58
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-01-27 17:16:31 -0800

    save

--- tests/acceptance/BasicCest.php
+++ tests/acceptance/BasicCest.php
@@ -3,17 +3,53 @@
 namespace Tests\Acceptance;
 
 use Tests\Support\AcceptanceTester;
-
 class BasicCest
 {
     public function _before(AcceptanceTester $I)
     {
 
     }
-
-    public function homepageIsVisible(AcceptanceTester $I)
+    public function homePageIsVisible(AcceptanceTester $I)
     {
         $I->amOnPage("/");
         $I->see("Mycodelic Forest");
+        $I->takeFullPageScreenshot("home-page");
     }
+
+    public function aboutPageIsVisible(AcceptanceTester $I)
+    {
+        $I->amOnPage("/contact");
+        $I->see("Contact Us");
+        $I->takeFullPageScreenshot("contact-page");
+    }
+
+    public function faqsPageIsVisible(AcceptanceTester $I)
+    {
+        $I->amOnPage("/faqs");
+        $I->see("FAQs");
+        $I->takeFullPageScreenshot("faqs-page");
+    }
+
+    public function resourcesPageIsVisible(AcceptanceTester $I)
+    {
+        $I->amOnPage("/resources");
+        $I->see("Resources");
+        $I->takeFullPageScreenshot("resources-page");
+    }
+
+    public function registerPageIsVisible(AcceptanceTester $I)
+    {
+        $I->amOnPage("/register");
+        $I->see("Register");
+        $I->takeFullPageScreenshot("register-page");
+    }
+
+    public function contactPageIsVisible(AcceptanceTester $I)
+    {
+        $I->amOnPage("/contact");
+        $I->see("Contact Us");
+        $I->takeFullPageScreenshot("contact-page");
+    }
+
+
 }
