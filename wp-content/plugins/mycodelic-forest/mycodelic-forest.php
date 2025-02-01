89955035731f6f555be7a9294a1b2ca497f258d9
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-02-01 12:56:01 -0800

    Update mycodelic-forest.php

--- wp-content/plugins/mycodelic-forest/mycodelic-forest.php
+++ wp-content/plugins/mycodelic-forest/mycodelic-forest.php
@@ -14,23 +14,29 @@ define('MYCO_CORE_ABS_PATH', WP_CONTENT_DIR . "/plugins/mycodelic-forest/");
 require_once(MYCO_CORE_ABS_PATH . 'classes/class-core.php');
 require_once(MYCO_CORE_ABS_PATH . 'classes/class-discord.php');
 require_once(MYCO_CORE_ABS_PATH . 'classes/class-forms.php');
-require_once(MYCO_CORE_ABS_PATH . 'classes/class-shortcodes.php');
+require_once(MYCO_CORE_ABS_PATH . 'classes/class-install.php');
 require_once(MYCO_CORE_ABS_PATH . 'classes/class-profile.php');
+require_once(MYCO_CORE_ABS_PATH . 'classes/class-roles.php');
+require_once(MYCO_CORE_ABS_PATH . 'classes/class-shortcodes.php');
  
 class MycodelicForestInit {
     public $version = '0.0.1';
     public $MycodelicForestCore;
     public $MycodelicForestDiscord;
     public $MycodelicForestForms;
-    public $MycodelicForestShortcodes;
+    public $MycodelicForestInstall;
     public $MycodelicForestProfile;
+    public $MycodelicForestRoles;
+    public $MycodelicForestShortcodes;
 
     public function __construct() {
         $this->MycodelicForestCore = new MycodelicForestCore();
         $this->MycodelicForestDiscord = new MycodelicForestDiscord();
         $this->MycodelicForestForms = new MycodelicForestForms();
-        $this->MycodelicForestShortcodes = new MycodelicForestShortcodes();
+        $this->MycodelicForestInstall = new MycodelicForestInstall();
         $this->MycodelicForestProfile = new MycodelicForestProfile();
+        $this->MycodelicForestRoles = new MycodelicForestRoles();
+        $this->MycodelicForestShortcodes = new MycodelicForestShortcodes();
     }
 
     public function init()
@@ -38,8 +44,10 @@ class MycodelicForestInit {
         $this->MycodelicForestCore->init();
         $this->MycodelicForestDiscord->init();
         $this->MycodelicForestForms->init();
-        $this->MycodelicForestShortcodes->init();
+        $this->MycodelicForestInstall->init();
         $this->MycodelicForestProfile->init();
+        $this->MycodelicForestRoles->init();
+        $this->MycodelicForestShortcodes->init();
     }
 }
 
