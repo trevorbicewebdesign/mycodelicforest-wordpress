e337da0212eb9c9ce38223d40995f2940469d012
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-02-28 15:22:47 -0800

    Update class-core.php

--- wp-content/plugins/mycodelic-forest/classes/class-core.php
+++ wp-content/plugins/mycodelic-forest/classes/class-core.php
@@ -8,6 +8,13 @@ class MycodelicForestCore {
 
     public function init()
     {
+        register_activation_hook(__FILE__, function() {
+            flush_rewrite_rules();
+        });
+        
+        register_deactivation_hook(__FILE__, function() {
+            flush_rewrite_rules();
+        });        
         
     }
 
