2c8fbe408dd29f0cbdf72d97896ad62393814764
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-03-01 15:28:50 -0800

    Update ProfileCest.php

--- tests/acceptance/ProfileCest.php
+++ tests/acceptance/ProfileCest.php
@@ -5,9 +5,10 @@ namespace Tests\Acceptance;
 use Tests\Support\AcceptanceTester;
 class ProfileCest
 {
+    protected $userId;
     public function _before(AcceptanceTester $I)
     {
-        $I->haveUserInDatabase("testuser", "subscriber",[
+        $this->userId = $I->haveUserInDatabase("testuser", "subscriber",[
             "first_name" => "Test",
             "last_name" => "User",
             "user_phone" => "123-456-7890",
@@ -54,7 +55,7 @@ class ProfileCest
         $I->fillField("#input_6_9_3", "Anytown");
         $I->fillField("#input_6_9_4", "CA");
         $I->fillField("#input_6_9_5", "12345");
-        $I->selectOption("#input_6_9_6", ["value" => "US"]);
+        $I->selectOption("#input_6_9_6", ["value" => "United States"]);
         $I->fillField("#input_6_13", "This is a test.");
         $I->fillField("#input_6_6", "TestBurner");
         $I->click("#choice_6_19_1");
@@ -71,12 +72,27 @@ class ProfileCest
         $I->seeInField("#input_6_9_3", "Anytown");
         $I->seeInField("#input_6_9_4", "CA");
         $I->seeInField("#input_6_9_5", "12345");
-        $I->seeOptionIsSelected("#input_6_9_6", "US");
+        $I->seeOptionIsSelected("#input_6_9_6", "United States");
         $I->seeInField("#input_6_13", "This is a test.");
         $I->seeInField("#input_6_6", "TestBurner");
         $I->seeCheckboxIsChecked("#choice_6_19_1");
 
-        
+        $I->see("Profile updated successfully!");
+
+        $I->seeInDatabase("wp_usermeta", ["user_id"=>$this->userId, "meta_key" => "first_name","meta_value" => "Test"]);
+        $I->seeInDatabase("wp_usermeta", ["user_id"=>$this->userId, "meta_key" => "last_name","meta_value" => "User"]);
+        $I->seeInDatabase("wp_usermeta", ["user_id"=>$this->userId, "meta_key" => "user_phone","meta_value" => "(555) 555-5555"]);
+        $I->seeInDatabase("wp_usermeta", ["user_id"=>$this->userId, "meta_key" => "address_1","meta_value" => "123 Main St"]);
+        $I->seeInDatabase("wp_usermeta", ["user_id"=>$this->userId, "meta_key" => "city","meta_value" => "Anytown"]);
+        $I->seeInDatabase("wp_usermeta", ["user_id"=>$this->userId, "meta_key" => "state","meta_value" => "CA"]);
+        $I->seeInDatabase("wp_usermeta", ["user_id"=>$this->userId, "meta_key" => "zip","meta_value" => "12345"]);
+        $I->seeInDatabase("wp_usermeta", ["user_id"=>$this->userId, "meta_key" => "country","meta_value" => "United States"]);
+        $I->seeInDatabase("wp_usermeta", ["user_id"=>$this->userId, "meta_key" => "user_about_me","meta_value" => "This is a test."]);
+        $I->seeInDatabase("wp_usermeta", ["user_id"=>$this->userId, "meta_key" => "playa_name","meta_value" => "TestBurner"]);
+        $I->seeInDatabase("wp_usermeta", ["user_id"=>$this->userId, "meta_key" => "has_attended_burning_man","meta_value" => "No"]);
+
+        $I->seeInDatabase("wp_users", ["ID" => $this->userId, "user_email" => "testuser@example.com"]);
+
    
     }
 }
