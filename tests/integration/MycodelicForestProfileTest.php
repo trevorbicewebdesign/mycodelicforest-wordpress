abd5ae3b8f2213db241ef03bdefbe6a873e2639e
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-03-02 15:51:36 -0800

    Update MycodelicForestProfileTest.php

--- tests/integration/MycodelicForestProfileTest.php
+++ tests/integration/MycodelicForestProfileTest.php
@@ -6,21 +6,39 @@ class MycodelicForestProfileTest extends \lucatume\WPBrowser\TestCase\WPTestCase
      * @var \IntegrationTester
      */
 
+     protected $wpid;
     protected function _before()
     {
-        //$this->user
+
+        $this->wpid = wp_insert_user([
+            'user_login' => 'testuser',
+            'user_pass' => 'password',
+            'user_email' => 'test.smith@mailinator.com'
+        ]);
+        
+        update_user_meta($this->wpid, 'first_name', 'Test');
+        update_user_meta($this->wpid, 'last_name', 'User');
+        update_user_meta($this->wpid, 'user_phone', '555-555-5555');
+        update_user_meta($this->wpid, 'address_1', '123 Main St');
+        update_user_meta($this->wpid, 'city', 'Anytown');
+        update_user_meta($this->wpid, 'state', 'CA');
+        update_user_meta($this->wpid, 'zip', '12345');
+        update_user_meta($this->wpid, 'country', 'United States');
+        update_user_meta($this->wpid, 'user_about_me', 'This is a test.');
+        update_user_meta($this->wpid, 'playa_name', 'TestBurner');
+        update_user_meta($this->wpid, 'has_attended_burning_man', 'Yes');
+        update_user_meta($this->wpid, 'years_attended', '["2024"]');
+
     }
 
     public function testProfileComplete()
     {
-        $wpid = 1;
-
         $MycodelicForestProfile = $this->make('MycodelicForestProfile', []);
 
-        $results = $MycodelicForestProfile->profileComplete($wpid);
+        $results = $MycodelicForestProfile->profileComplete($this->wpid);
         codecept_debug($results);
 
-        $this->assertFalse($results);
+        $this->assertTrue($results);
     }
 
 }
