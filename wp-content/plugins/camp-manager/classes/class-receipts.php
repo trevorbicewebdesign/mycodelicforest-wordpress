98f9bae6aaba5aee0e2b7108517d8dff5c017ebf
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-06-24 17:53:22 -0700

    Update class-receipts.php

--- wp-content/plugins/camp-manager/classes/class-receipts.php
+++ wp-content/plugins/camp-manager/classes/class-receipts.php
@@ -52,6 +52,14 @@ class CampManagerReceipts
         $table->process_bulk_action();
         $table->prepare_items();
         ?>
+        <style>
+            .wp-list-table .column-store       { width: 40%; }
+            .wp-list-table .column-date        { width: 15%; }
+            .wp-list-table .column-total       { width: 10%; text-align: right; }
+            .wp-list-table .column-subtotal    { width: 10%; text-align: right; }
+            .wp-list-table .column-tax         { width: 10%; text-align: right; }
+            .wp-list-table .column-shipping    { width: 15%; text-align: right; }
+        </style>
         <div class="wrap">
             <h1 class="wp-heading-inline">Receipts</h1>
             <a href="<?php echo admin_url('admin.php?page=camp-manager-upload-receipt'); ?>" class="page-title-action">Add New</a>
