d69329ec8e3af35d639ee56a68e98db5252d5bc3
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-06-24 18:46:14 -0700

    Create class-roster.php

---
+++ wp-content/plugins/camp-manager/classes/class-roster.php
@@ -0,0 +1,34 @@
+<?php
+
+class CampManagerRoster
+{
+
+    public function __construct()
+    {
+        // Constructor logic if needed
+    }
+
+    public function init()
+    {
+        // Initialization logic for the roster
+        // This could include setting up database connections, loading necessary libraries, etc.
+    }
+
+    public function getRosterMembers(): array
+    {
+        // Placeholder for fetching roster members
+        return [];
+    }
+
+    public function addMember($memberData)
+    {
+        // Placeholder for adding a member to the roster
+        return true;
+    }
+
+    public function removeMember($memberId)
+    {
+        // Placeholder for removing a member from the roster
+        return true;
+    }
+}
