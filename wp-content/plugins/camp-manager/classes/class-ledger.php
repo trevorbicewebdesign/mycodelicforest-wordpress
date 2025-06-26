3337f89a16f58710cd6c6599f0eaeb11bc6e180a
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-06-25 20:24:23 -0700

    Update class-ledger.php

--- wp-content/plugins/camp-manager/classes/class-ledger.php
+++ wp-content/plugins/camp-manager/classes/class-ledger.php
@@ -10,15 +10,12 @@ class CampManagerLedger
 
     public function init()
     {
-        // Initialization logic for the ledger
-        // This could include setting up database connections, loading necessary libraries, etc.
+
     }
 
     public function record_money_in($amount, $description = '', $date = null)
     {
-        // Record money coming in
-        // This could be a database insert or an API call
-        // For now, just a placeholder
+
         return [
             'type' => 'money_in',
             'amount' => $amount,
@@ -29,9 +26,7 @@ class CampManagerLedger
 
     public function record_money_out($amount, $description = '', $date = null)
     {
-        // Record money going out
-        // This could be a database insert or an API call
-        // For now, just a placeholder
+
         return [
             'type' => 'money_out',
             'amount' => $amount,
