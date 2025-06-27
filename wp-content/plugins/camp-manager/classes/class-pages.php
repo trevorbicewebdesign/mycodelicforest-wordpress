42af1eac2b77e1b45d2673d3a78075bb92d90d87
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-06-26 19:37:55 -0700

    Update class-pages.php

--- wp-content/plugins/camp-manager/classes/class-pages.php
+++ wp-content/plugins/camp-manager/classes/class-pages.php
@@ -469,7 +469,7 @@ class CampManagerPages
             <h1 class="wp-heading-inline">Ledger</h1>
             <a href="<?php echo admin_url('admin.php?page=camp-manager-add-ledger'); ?>" class="page-title-action">Add New</a>
             <hr class="wp-header-end">
-            <h3>The current amount is: <?php echo esc_html($table->get_total_amount()); ?></h3>
+            <h3>The current balance is: <?php echo esc_html($table->get_total_amount()); ?></h3>
             <h3>We have collected <?php echo esc_html($table->get_total_camp_dues()); ?> in camp dues</h3>
             <form method="post">
                 <?php
