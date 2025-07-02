9798bde847543cc2e182cd818c34c871279c1e3e
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-07-01 19:16:13 -0700

    Create camp_manager_dashboard.php

---
+++ wp-content/plugins/camp-manager/tmpl/camp_manager_dashboard.php
@@ -0,0 +1,27 @@
+<div class="wrap">
+    <h1>Camp Manager Dashboard</h1>
+    <hr>
+    <h3>Camp Dues Collected: </h3>
+    <h3>Donations Collected: </h3>
+    <h3>Revenue Spent: </h3>
+    <h3>PayPal Balance: </h3>
+    <h3>Remaining Funds: </h3>
+    <h3>Starting Funds: </h3>
+    <h3>Unreimbursed Expenses: </h3>
+    <hr>
+    <h3>Collected Revenue: </h3>
+    <h3>Estimated Revenue: </h3>
+    <h3>Estimated Fund Remaining: </h3>
+    <h3>Unallocated Funds: </h3>
+    <hr/>
+    <h3>Camp Dues: 350.00</h3>
+    <h3>Low Income Camp Dues: 250.00</h3>
+    <h3>Low Income Camp Dues Count: </h3>
+    <h3># Camp Members: </h3>
+    <h3>Camp Dues Collected: </h3>
+    <h3>Camp Dues Remaining: </h3>
+    <h3>Low Income Camp Dues Remaining: </h3>
+    <h3>Total Camp Dues: </h3>
+    <h3>Camp Dues Left To Collect: </h3>
+
+</div>
