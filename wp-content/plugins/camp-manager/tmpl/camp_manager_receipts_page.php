0072127280e99793f59241b4d5cb5b2c2b982dd7
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-07-02 13:48:00 -0700

    Update camp_manager_receipts_page.php

--- wp-content/plugins/camp-manager/tmpl/camp_manager_receipts_page.php
+++ wp-content/plugins/camp-manager/tmpl/camp_manager_receipts_page.php
@@ -1,40 +1,24 @@
 <?php
 
-echo '<div class="wrap"><h1>Receipts</h1><a href="/wp-admin/admin.php?page=camp-manager-upload-receipt" class="page-title-action">Upload Receipt</a>';
-
-$receipts = $this->get_receipts();
-
-if ($receipts && count($receipts) > 0) {
-    echo '<div class="wp-list-table widefat fixed striped posts">';
-    echo '<table>';
-    echo '<thead>
-            <tr>
-                <th scope="col" class="manage-column column-title column-primary">Store</th>
-                <th scope="col" class="manage-column">Date</th>
-                <th scope="col" class="manage-column">Subtotal</th>
-                <th scope="col" class="manage-column">Tax</th>
-                <th scope="col" class="manage-column">Shipping</th>
-                <th scope="col" class="manage-column">Total</th>
-            </tr>
-            </thead>
-            <tbody>';
-    foreach ($receipts as $receipt) {
-        echo '<tr>';
-        echo '<td class="column-title has-row-actions column-primary" data-colname="Store">'
-            . '<strong>' . esc_html($receipt->store) . '</strong>'
-            . '<button type="button" class="toggle-row"><span class="screen-reader-text">Show more details</span></button>'
-            . '</td>';
-        echo '<td data-colname="Date">' . esc_html($receipt->date) . '</td>';
-        echo '<td data-colname="Subtotal">' . esc_html($receipt->subtotal) . '</td>';
-        echo '<td data-colname="Tax">' . esc_html($receipt->tax) . '</td>';
-        echo '<td data-colname="Shipping">' . esc_html($receipt->shipping) . '</td>';
-        echo '<td data-colname="Total">' . esc_html($receipt->total) . '</td>';
-        echo '</tr>';
-    }
-    echo '</tbody></table></div>';
-} else {
-    echo '<p>No receipts found.</p>';
-}
-echo '</div>';
-
+$table = new CampManagerReceiptsTable();
+$table->process_bulk_action();
+$table->prepare_items();
 ?>
+<style>
+    .wp-list-table .column-store       { width: 40%; }
+    .wp-list-table .column-date        { width: 15%; }
+    .wp-list-table .column-total       { width: 10%; text-align: right; }
+    .wp-list-table .column-subtotal    { width: 10%; text-align: right; }
+    .wp-list-table .column-tax         { width: 10%; text-align: right; }
+    .wp-list-table .column-shipping    { width: 15%; text-align: right; }
+</style>
+<div class="wrap">
+    <h1 class="wp-heading-inline">Receipts</h1>
+    <a href="<?php echo admin_url('admin.php?page=camp-manager-add-receipt'); ?>" class="page-title-action">Add New</a>
+    <hr class="wp-header-end">
+    <form method="post">
+        <?php
+        $table->display();
+        ?>
+    </form>
+</div>
