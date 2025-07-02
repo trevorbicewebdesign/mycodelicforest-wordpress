05764da0ab9db28614bee214626a5e74b220fcb3
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-07-01 23:45:49 -0700

    Update camp_manager_dashboard.php

--- wp-content/plugins/camp-manager/tmpl/camp_manager_dashboard.php
+++ wp-content/plugins/camp-manager/tmpl/camp_manager_dashboard.php
@@ -1,6 +1,22 @@
 <?php
 $CampManagerLedger = new CampManagerLedger();
+$CampManagerRoster = new CampManagerRoster();
 
+$starting_balance = $CampManagerLedger->startingBalance();
+
+$total_camp_dues = $CampManagerLedger->totalCampDues();
+$total_donations = $CampManagerLedger->totalDonations();
+$total_revenue = $CampManagerLedger->totalMoneyIn();
+$total_expenses = $CampManagerLedger->totalMoneyOut();
+
+$paypal_balance = $starting_banace + $total_revenue - $total_expenses;
+
+$total_camp_members = $CampManagerRoster->countRosterMembers();
+$total_low_income_members = $CampManagerRoster->countLowIncomeMembers();
+$total_paid_camp_dues = $CampManagerRoster->countPaidCampDues();
+$total_paid_low_income_camp_dues = $CampManagerRoster->countPaidLowIncomeCampDues();
+
+$total_unpaid_camp_dues = $CampManagerRoster->countUnpaidCampDues()
 ?>
 <div class="wrap">
     <h1>Camp Manager Dashboard</h1>
@@ -8,27 +24,27 @@ $CampManagerLedger = new CampManagerLedger();
 
     <div style="display: flex; gap: 2rem; margin-bottom: 2rem;">
         <div style="flex: 1;">
-            <h3>Camp Dues Collected: <?php echo $CampManagerLedger->totalCampDues(); ?></h3>
-            <h3>Donations Collected: <?php echo $CampManagerLedger->totalDonations(); ?></h3>
-            <h3>Revenue Spent: </h3>
-            <h3>PayPal Balance: </h3>
-            <h3>Remaining Funds: </h3>
-            <h3>Starting Funds: </h3>
+            <h3>Camp Dues Collected: $<?php echo number_format($total_camp_dues, 2); ?></h3>
+            <h3>Donations Collected: $<?php echo number_format($total_donations, 2); ?></h3>
+            <h3>Revenue Spent: $<?php echo number_format($total_expenses, 2); ?></h3>
+            <h3>PayPal Balance: $<?php echo number_format($paypal_balance + $total_revenue - $total_expenses, 2); ?></h3>
+            <h3>Remaining Funds: $<?php echo number_format($total_revenue - $total_expenses, 2); ?></h3>
+            <h3>Starting Funds: $<?php echo $starting_balance; ?></h3>
             <h3>Unreimbursed Expenses: </h3>
             <hr>
-            <h3>Collected Revenue: </h3>
-            <h3>Estimated Revenue: </h3>
+            <h3>Collected Revenue: <?php echo $total_revenue; ?></h3>
+            <h3>Estimated Revenue: <?php echo ($total_low_income_members * 250) + (($total_camp_members - $total_low_income_members) * 350); ?></h3>
             <h3>Estimated Fund Remaining: </h3>
             <h3>Unallocated Funds: </h3>
             <hr/>
             <h3>Camp Dues: 350.00</h3>
             <h3>Low Income Camp Dues: 250.00</h3>
-            <h3>Low Income Camp Dues Count: </h3>
-            <h3># Camp Members: </h3>
-            <h3>Camp Dues Collected: </h3>
-            <h3>Camp Dues Remaining: </h3>
-            <h3>Low Income Camp Dues Remaining: </h3>
-            <h3>Total Camp Dues: </h3>
+            <h3>Low Income Camp Dues Count: <?php echo $total_low_income_members; ?></h3>
+            <h3># Camp Members: <?php echo $total_camp_members; ?></h3>
+            <h3>Camp Dues Collected: <?php echo $total_camp_dues; ?></h3>
+            <h3>Camp Dues Remaining: <?php echo $total_unpaid_camp_dues; ?></h3>
+            <h3>Low Income Camp Dues Remaining: <?php echo $total_paid_low_income_camp_dues; ?></h3>
+            <h3>Total Camp Dues: <?php echo $total_paid_camp_dues; ?></h3>
             <h3>Camp Dues Left To Collect: </h3>
         </div>
         <div style="flex: 3;">
