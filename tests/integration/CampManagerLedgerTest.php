78f2c206bb73b3b5d4fe28447dbb1cefc34ada08
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-07-04 14:31:27 -0700

    Update CampManagerLedgerTest.php

--- tests/integration/CampManagerLedgerTest.php
+++ tests/integration/CampManagerLedgerTest.php
@@ -12,4 +12,46 @@ class CampManagerLedgerTest extends \lucatume\WPBrowser\TestCase\WPTestCase
         
     }
 
+    public function testUpdateLedger()
+    {
+        $ledger_id = $this->tester->haveInDatabase('wp_mf_ledger', [
+            'amount' => 50.00,
+            'note' => 'Initial Ledger Entry',
+            'date' => '2023-09-01',
+        ]);
+
+        $CampManagerLedger = $this->make('CampManagerLedger', []);
+        
+        $data = [
+            'amount' => 100.00,
+            'note' => 'Test Ledger Entry',
+            'date' => '2023-10-01',
+            'line_items' => []
+        ];
+        
+        $CampManagerLedger->updateLedger($ledger_id, $data);
+
+    }
+
+    public function testInsertLedgerLineItem()
+    {
+        $ledger_id = $this->tester->haveInDatabase('wp_mf_ledger', [
+            'amount' => 50.00,
+            'note' => 'Initial Ledger Entry',
+            'date' => '2023-09-01',
+        ]);
+
+        $CampManagerLedger = $this->make('CampManagerLedger', []);
+        
+        $data = [
+            'amount' => 25.00,
+            'note' => 'Test Line Item',
+            'date' => '2023-10-01',
+            'receipt_id' => null,
+            'type' => 'Expense'
+        ];
+        
+        $CampManagerLedger->insertLedgerLineItem($ledger_id, $data);
+    }
+
 }
