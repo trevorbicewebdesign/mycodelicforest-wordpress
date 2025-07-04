dffa554cb4539ec2d2ede9442ed20b0dfa575fd4
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-07-03 18:34:28 -0700

    Update class-pages.php

--- wp-content/plugins/camp-manager/classes/class-pages.php
+++ wp-content/plugins/camp-manager/classes/class-pages.php
@@ -518,11 +518,23 @@ class CampManagerPages
             .wp-list-table .column-subtotal    { width: 10%; text-align: right; }
             .wp-list-table .column-tax         { width: 10%; text-align: right; }
             .wp-list-table .column-shipping    { width: 15%; text-align: right; }
+
+            .paid-row {
+                background-color: #e6ffea !important;
+            }
+            .unpaid-row {
+                background-color: #ffe6e6 !important;
+            }
         </style>
         <div class="wrap">
             <h1 class="wp-heading-inline">Roster</h1>
             <a href="<?php echo admin_url('admin.php?page=camp-manager-add-member'); ?>" class="page-title-action">Add New</a>
             <hr class="wp-header-end">
+            <h3>Total Camp Members: <?php echo $this->roster->countRosterMembers(); ?></h3>
+            <h4>Low Income Camp Members: <?php echo $this->roster->countLowIncomeMembers(); ?></h4>
+            <h3>Total Camp Dues: <?php echo $this->ledger->totalCampDues(); ?></h3>
+            <h3>Low Income Camp Dues Paid: <?php echo $this->ledger->sumUserCampDues(0); ?></h3>
+
             <form method="post">
                 <?php
                 $table->display();
