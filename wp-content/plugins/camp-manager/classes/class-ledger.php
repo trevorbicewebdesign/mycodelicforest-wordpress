210c033645dd95cc95952684dd558ebb87a13c5b
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-07-04 18:29:33 -0700

    Update class-ledger.php

--- wp-content/plugins/camp-manager/classes/class-ledger.php
+++ wp-content/plugins/camp-manager/classes/class-ledger.php
@@ -220,24 +220,4 @@ class CampManagerLedger
         global $wpdb;
         return $wpdb->get_var($wpdb->prepare("SELECT SUM(amount) FROM {$wpdb->prefix}mf_ledger_line_items WHERE cmid = %d AND (type = 'Camp Dues' OR type = 'Partial Camp Dues')", $cmid)) ?: 0;
     }
-
-    public function record_money_in($amount, $description = '', $date = null)
-    {
-        return [
-            'type' => 'money_in',
-            'amount' => $amount,
-            'description' => $description,
-            'date' => $date ?: date('Y-m-d H:i:s'),
-        ];
-    }
-
-    public function record_money_out($amount, $description = '', $date = null)
-    {
-        return [
-            'type' => 'money_out',
-            'amount' => $amount,
-            'description' => $description,
-            'date' => $date ?: date('Y-m-d H:i:s'),
-        ];
-    }
 }
