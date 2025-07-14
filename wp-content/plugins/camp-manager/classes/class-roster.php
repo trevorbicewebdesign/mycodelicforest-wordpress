da83f417ab722285df3729766dc50a17c1de2302
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-07-14 11:32:56 -0700

    Update class-roster.php

--- wp-content/plugins/camp-manager/classes/class-roster.php
+++ wp-content/plugins/camp-manager/classes/class-roster.php
@@ -29,9 +29,9 @@ class CampManagerRoster
                 'playaname' => sanitize_text_field($_POST['member_playaname']),
                 'email' => sanitize_email($_POST['member_email']),
                 //'wpid' => get_current_user_id(),
-                //'low_income' => isset($_POST['low_income']) ? (int)$_POST['low_income'] : null,
-                //'fully_paid' => isset($_POST['fully_paid']) ? (int)$_POST['fully_paid'] : null,
-                //'season' => isset($_POST['season']) ? (int)$_POST['season'] : null,
+                'low_income' => isset($_POST['low_income']) ? (int)$_POST['low_income'] : null,
+                'fully_paid' => isset($_POST['fully_paid']) ? (int)$_POST['fully_paid'] : null,
+                'season' => isset($_POST['season']) ? (int)$_POST['season'] : null,
             ]);
             
         } catch (\Exception $e) {
@@ -111,11 +111,11 @@ class CampManagerRoster
             'wpid' => $memberData['wpid'],
             'low_income' => isset($memberData['low_income']) ? (int)$memberData['low_income'] : null,
             'fully_paid' => isset($memberData['fully_paid']) ? (int)$memberData['fully_paid'] : null,
-            'season' => isset($memberData['season']) ? (int)$memberData['season'] : null,
+            'season' => 2025,
             'fname' => sanitize_text_field($memberData['fname']),
             'lname' => sanitize_text_field($memberData['lname']),
             'playaname' => sanitize_text_field($memberData['playaname']),
-            // 'email' => sanitize_email($memberData['email']),
+            'email' => sanitize_email($memberData['email']),
         ];
 
         
