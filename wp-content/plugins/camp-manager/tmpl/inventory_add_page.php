ca4c69ed8824a2925a7383ad6bddeed7180bb3a0
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-08-06 15:01:09 -0700

    Update inventory_add_page.php

--- wp-content/plugins/camp-manager/tmpl/inventory_add_page.php
+++ wp-content/plugins/camp-manager/tmpl/inventory_add_page.php
@@ -113,6 +113,14 @@ $inventory_id = $is_edit ? intval($inventory->id) : 0;
             <?php submit_button('Save & Close Inventory', 'primary', 'save_close_receipt', false, array('id' => 'save-close-btn')); ?>
             <?php submit_button('Close Inventory', 'secondary', 'close_receipt', false, array('id' => 'close-btn', 'formnovalidate' => true)); ?>
         </div>
+
+        <?php
+        $return_url = '';
+        if (!empty($_REQUEST['return'])) {
+            $return_url = $_REQUEST['return'];
+        }
+        ?>
+        <input type="hidden" name="return_url" value="<?php echo esc_attr($return_url); ?>">
     </form>
 </div>
 <script type="text/javascript">
